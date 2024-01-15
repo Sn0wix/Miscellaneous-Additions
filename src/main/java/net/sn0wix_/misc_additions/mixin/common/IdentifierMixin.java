7608db1845d5b2345c22ed2c382cfe918c990af0
Sn0wix_ <zerochipmunk317@centrum.cz>
2024-01-15 17:20:26 +0100

    modid change fixer

---
+++ src/main/java/net/sn0wix_/misc_additions/mixin/common/IdentifierMixin.java
@@ -0,0 +1,24 @@
+package net.sn0wix_.misc_additions.mixin.common;
+
+import net.minecraft.util.Identifier;
+import net.sn0wix_.misc_additions.common.MiscAdditions;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Identifier.class)
+public abstract class IdentifierMixin {
+    @Mutable
+    @Shadow @Final private String namespace;
+
+    @Inject(method = "<init>(Ljava/lang/String;Ljava/lang/String;)V", at = @At("RETURN"))
+    private void injectConstructor(String namespace, String path, CallbackInfo ci) {
+        if (namespace.equals("villagepillagearise")) {
+            this.namespace = MiscAdditions.MOD_ID;
+        }
+    }
+}
